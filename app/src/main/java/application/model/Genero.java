0263e8cff2cb31fb557b58a11cb52fac61ae3783
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-10-30 21:18:08 +0000

    biblioteca pronta

--- app/src/main/java/application/model/Genero.java
+++ app/src/main/java/application/model/Genero.java
@@ -17,7 +17,7 @@ public class Genero {
     public int getId() {
         return id;
     }
-    public void setId (ind id) {
+    public void setId (int id) {
         this.id = id;
     }
     public String getNome() {
